6cf134887daea82985c7c04d3aa06f7545677797
Santiago Graviano <santiago.graviano@gmail.com>
2016-06-17 19:17:55 -0300

    First commit

---
+++ src/hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'Hello';
+    }
+    render() {
+        return <div>Hello</div>;
+    }
+}
+
+export default Hello;
